392d7f339eb4bf8b954e8b0328ff6b2d0b470344
Shubham Chatterjee <c.suvam@gmail.com>
2020-12-21 09:51:32 +0530

    code updates

--- src/components/Bottomsection.js
+++ src/components/Bottomsection.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import '../CSS/background.css';
+import cross from '../assets/icon_cross.PNG';
+import { Typography } from '@material-ui/core';
 
 export default function BottomSection() {
+
     return (
-        <div>
-            
+        <div className='bot_section'>
+            <div id='insideBot'>
+                <div className='flexAdder'>
+                    <Typography>
+                        Users
+                    </Typography>
+                    <Typography>
+                        Users
+                    </Typography>
+                    <Typography>
+                        Users
+                    </Typography>
+                    <Typography>
+                        Users
+                    </Typography>
+                    <Typography>
+                        Users
+                    </Typography>
+                </div>
+            </div>
         </div>
     )
 }
